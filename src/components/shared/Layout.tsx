b57553a312717a3515e9e111e02f124e7cd774f6
derey79 <devyreymond@gmail.com>
2026-07-05 22:14:07 +0700

    update layout pages

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -4,10 +4,8 @@ import Navbar from './Navbar';
 export default function Layout() {
   return (
     <div className='min-h-screen bg-base-background'>
-      {/* 1. Lapisan Navigasi Atas */}
       <Navbar />
 
-      {/* 2. Area Konten Utama - Menggunakan Lebar Penuh Responsif (Satu Kolom) */}
       <main className='custom-container py-8 px-4 mx-auto'>
         <div className='flex flex-col space-y-8 w-full'>
           <Outlet />
