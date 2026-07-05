7bb7a97c6c9c9c4d776e72aeb0ca86d2755315ad
derey79 <devyreymond@gmail.com>
2026-07-05 22:18:04 +0700

    modified file for update logo

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,5 +1,6 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import logo from '@/assets/logo-booky.png';
 import * as z from 'zod';
 import { useRegisterMutation } from '../hooks/useAuthMutation';
 
@@ -38,140 +39,158 @@ export const RegisterForm = () => {
   });
 
   return (
-    <Card className='w-full max-w-md mx-auto shadow-lg'>
-      <CardHeader>
-        <CardTitle className='text-2xl font-bold text-center'>
-          Create Account
-        </CardTitle>
-        <CardDescription className='text-center'>
-          Register your account for E-Library
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        {/* Hubungkan langsung handler submit ke form HTML asli */}
-        <form onSubmit={handleSubmit((v) => mutate(v))} className='space-y-4'>
-          {/* FIELD NAME */}
-          <div className='space-y-2'>
-            <Label
-              htmlFor='name'
-              className={errors.name ? 'text-destructive' : ''}
-            >
-              Name
-            </Label>
-            <Input
-              id='name'
-              placeholder='John Doe'
-              className={
-                errors.name
-                  ? 'border-destructive focus-visible:ring-destructive'
-                  : ''
-              }
-              disabled={isPending}
-              {...register('name')} // 💡 Registrasi langsung ke react-hook-form
+    <div className='flex min-h-[80vh] flex-col items-center justify-center px-4 py-12 sm:px-6 lg:px-8'>
+      <Card className='w-full max-w-md mx-auto shadow-lg'>
+        <CardHeader className='space-y-1.5 px-6 pt-6 text-left'>
+          <div className='flex items-center gap-3 pb-4'>
+            <img
+              src={logo}
+              alt='Company Logo'
+              className='h-full w-auto object-contain'
             />
-            {errors.name && (
-              <p className='text-xs font-medium text-destructive'>
-                {errors.name.message}
-              </p>
-            )}
+            <span className='text-2xl font-bold tracking-tighter space-y-6 text-main-color whitespace-nowrap select-none'>
+              Booky
+            </span>
           </div>
 
-          {/* FIELD EMAIL */}
-          <div className='space-y-2'>
-            <Label
-              htmlFor='email'
-              className={errors.email ? 'text-destructive' : ''}
-            >
-              Email
-            </Label>
-            <Input
-              id='email'
-              type='email'
-              placeholder='john@example.com'
-              className={
-                errors.email
-                  ? 'border-destructive focus-visible:ring-destructive'
-                  : ''
-              }
+          <CardTitle className='text-2xl font-quicksand font-bold text-left'>
+            Register
+          </CardTitle>
+          <CardDescription className='text-left'>
+            Create your account to start borrowing books.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          {/* Hubungkan langsung handler submit ke form HTML asli */}
+          <form onSubmit={handleSubmit((v) => mutate(v))} className='space-y-4'>
+            {/* FIELD NAME */}
+            <div className='space-y-2'>
+              <Label
+                htmlFor='name'
+                className={errors.name ? 'text-destructive' : ''}
+              >
+                Name
+              </Label>
+              <Input
+                id='name'
+                placeholder='John Doe'
+                className={
+                  errors.name
+                    ? 'border-destructive focus-visible:ring-destructive'
+                    : ''
+                }
+                disabled={isPending}
+                {...register('name')} // 💡 Registrasi langsung ke react-hook-form
+              />
+              {errors.name && (
+                <p className='text-xs font-medium text-destructive'>
+                  {errors.name.message}
+                </p>
+              )}
+            </div>
+
+            {/* FIELD EMAIL */}
+            <div className='space-y-2'>
+              <Label
+                htmlFor='email'
+                className={errors.email ? 'text-destructive' : ''}
+              >
+                Email
+              </Label>
+              <Input
+                id='email'
+                type='email'
+                placeholder='john@example.com'
+                className={
+                  errors.email
+                    ? 'border-destructive focus-visible:ring-destructive'
+                    : ''
+                }
+                disabled={isPending}
+                {...register('email')}
+              />
+              {errors.email && (
+                <p className='text-xs font-medium text-destructive'>
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+
+            {/* FIELD PHONE */}
+            <div className='space-y-2'>
+              <Label
+                htmlFor='phone'
+                className={errors.phone ? 'text-destructive' : ''}
+              >
+                Phone
+              </Label>
+              <Input
+                id='phone'
+                placeholder='081234...'
+                className={
+                  errors.phone
+                    ? 'border-destructive focus-visible:ring-destructive'
+                    : ''
+                }
+                disabled={isPending}
+                {...register('phone')}
+              />
+              {errors.phone && (
+                <p className='text-xs font-medium text-destructive'>
+                  {errors.phone.message}
+                </p>
+              )}
+            </div>
+
+            {/* FIELD PASSWORD */}
+            <div className='space-y-2'>
+              <Label
+                htmlFor='password'
+                className={errors.password ? 'text-destructive' : ''}
+              >
+                Password
+              </Label>
+              <Input
+                id='password'
+                type='password'
+                placeholder='••••••••'
+                className={
+                  errors.password
+                    ? 'border-destructive focus-visible:ring-destructive'
+                    : ''
+                }
+                disabled={isPending}
+                {...register('password')}
+              />
+              {errors.password && (
+                <p className='text-xs font-medium text-destructive'>
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <Button
+              type='submit'
+              className='w-full h-10 font-medium rounded-full bg-primary-300 hover:bg-primary-300/70 tracking-wide shadow-md mt-2'
               disabled={isPending}
-              {...register('email')}
-            />
-            {errors.email && (
-              <p className='text-xs font-medium text-destructive'>
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-
-          {/* FIELD PHONE */}
-          <div className='space-y-2'>
-            <Label
-              htmlFor='phone'
-              className={errors.phone ? 'text-destructive' : ''}
             >
-              Phone
-            </Label>
-            <Input
-              id='phone'
-              placeholder='081234...'
-              className={
-                errors.phone
-                  ? 'border-destructive focus-visible:ring-destructive'
-                  : ''
-              }
-              disabled={isPending}
-              {...register('phone')}
-            />
-            {errors.phone && (
-              <p className='text-xs font-medium text-destructive'>
-                {errors.phone.message}
-              </p>
-            )}
+              {isPending ? (
+                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+              ) : (
+                'Submit'
+              )}
+            </Button>
+          </form>
+
+          <div className='mt-4 text-center text-sm text-muted-foreground'>
+            Have an account?{' '}
+            <Link to='/login' className='text-primary hover:underline'>
+              Log in
+            </Link>
           </div>
-
-          {/* FIELD PASSWORD */}
-          <div className='space-y-2'>
-            <Label
-              htmlFor='password'
-              className={errors.password ? 'text-destructive' : ''}
-            >
-              Password
-            </Label>
-            <Input
-              id='password'
-              type='password'
-              placeholder='••••••••'
-              className={
-                errors.password
-                  ? 'border-destructive focus-visible:ring-destructive'
-                  : ''
-              }
-              disabled={isPending}
-              {...register('password')}
-            />
-            {errors.password && (
-              <p className='text-xs font-medium text-destructive'>
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-
-          <Button type='submit' className='w-full' disabled={isPending}>
-            {isPending ? (
-              <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-            ) : (
-              'Register'
-            )}
-          </Button>
-        </form>
-
-        <div className='mt-4 text-center text-sm text-muted-foreground'>
-          Have an account?{' '}
-          <Link to='/login' className='text-primary hover:underline'>
-            Log in
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
