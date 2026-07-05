f81b2865075ba575ee03a42f587a74c633107194
derey79 <devyreymond@gmail.com>
2026-07-05 22:28:33 +0700

    update routes, add import file for route link

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,7 +6,16 @@ import Layout from '@/components/shared/Layout';
 import LandingPage from '@/pages/LandingPage';
 import { LoginForm } from '@/features/auth/components/LoginForm';
 import { RegisterForm } from '@/features/auth/components/RegisterForm';
-import AdminDashboard from '@/features/admin/components/AdminDashboard';
+
+// import AdminDashboard from '@/features/admin/components/AdminDashboard';
+import AdminDashboardPage from '@/pages/AdminDashboardPage';
+import UserProfilePage from '@/pages/UserProfilePage';
+import BookDetail from '@/features/books/book-detail/BookDetail';
+// import MyLoans from '@/features/loans/MyLoans';
+import CartPage from '@/pages/CartPage';
+import AddBookPage from '@/pages/AddBookPage';
+import BookListCatalog from '@/features/books/components/BookListCatalog';
+import CheckoutPage from '@/pages/CheckoutPage';
 
 const AppRoutes = () => {
   return (
@@ -16,6 +25,8 @@ const AppRoutes = () => {
         <Route element={<Layout />}>
           {/* Akses beranda tetap memunculkan Navbar & Hero Section */}
           <Route path='/' element={<LandingPage />} />
+          <Route path='/books' element={<BookListCatalog />} />
+          <Route path='/books/:id' element={<BookDetail />} />
         </Route>
 
         {/* === KELOMPOK 2: AUTH PUBLIK (TANPA LAYOUT / FULL SCREEN) === */}
@@ -27,17 +38,26 @@ const AppRoutes = () => {
         <Route element={<ProtectedRoute allowedRoles={['USER', 'ADMIN']} />}>
           <Route element={<Layout />}>
             {/* Halaman katalog internal setelah user berhasil masuk */}
-            <Route
-              path='/books'
-              element={<div>Katalog Buku Internal (Under Construction)</div>}
-            />
+            {/* <Route path='/books' element={<LandingPage />} /> */}
+            <Route path='/' element={<LandingPage />} />
             {/* Anda bisa menambahkan rute internal lain di sini seperti /my-loans atau /profile */}
+            <Route path='/profile' element={<UserProfilePage />} />
+            <Route path='/cart' element={<CartPage />} />
+
+            <Route path='/checkout' element={<CheckoutPage />} />
+            {/* <Route path='/my-loans' element={<MyLoans />} /> */}
+            <Route
+              path='/profile'
+              element={<div>Halaman Profil (Under Construction)</div>}
+            />
           </Route>
         </Route>
 
         {/* === KELOMPOK 4: PANEL ADMIN TERPROTEKSI (MANDIRI / FULL SCREEN) === */}
         <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
-          <Route path='/admin/dashboard' element={<AdminDashboard />} />
+          {/* <Route path='/admin/dashboard' element={<AdminDashboard />} /> */}
+          <Route path='/admin/dashboard' element={<AdminDashboardPage />} />
+          <Route path='/admin/books/add' element={<AddBookPage />} />
         </Route>
 
         {/* Fallback otomatis jika pengguna mengetik URL asal */}
