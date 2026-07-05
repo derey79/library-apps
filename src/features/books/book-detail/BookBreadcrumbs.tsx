a43ca3a4913e46da82b2ed988a7015e87c3af7f8
derey79 <devyreymond@gmail.com>
2026-07-05 22:19:22 +0700

    update bread crumbs for link and color

--- src/features/books/book-detail/BookBreadcrumbs.tsx
+++ src/features/books/book-detail/BookBreadcrumbs.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { ChevronRight, Home } from 'lucide-react';
+import { ChevronRight } from 'lucide-react';
 
 interface BookBreadcrumbsProps {
   title: string;
@@ -7,13 +7,13 @@ interface BookBreadcrumbsProps {
 
 export default function BookBreadcrumbs({ title }: BookBreadcrumbsProps) {
   return (
-    <nav className='flex items-center gap-1.5 text-xs font-semibold text-neutral-400 select-none'>
+    <nav className='flex items-center gap-1.5 text-sm font-semibold text-primary-300 select-none px-4'>
       <Link to='/' className='hover:text-neutral-900 flex items-center gap-1'>
-        <Home className='h-3 w-3' />
+        {/* <Home className='h-3 w-3' /> */}
         Home
       </Link>
       <ChevronRight className='h-3 w-3 text-neutral-300' />
-      <span className='text-neutral-400'>Category</span>
+      <span className='text-primary-400'>Category</span>
       <ChevronRight className='h-3 w-3 text-neutral-300' />
       <span className='text-neutral-900 font-bold truncate max-w-50 sm:max-w-none'>
         {title}
