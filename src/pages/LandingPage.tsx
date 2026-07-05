1cb9fd7bf9bf7ac392238d269e4aa7b71dce856d
derey79 <devyreymond@gmail.com>
2026-07-05 22:27:08 +0700

    update landing page for link, add import page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,13 +1,24 @@
-import Hero from '@/features/books/componenst/Hero';
+import { useState } from 'react';
+import Hero from '@/features/books/components/Hero';
+import CategoryList from '@/features/books/components/CategoryList';
+import BookGrid from '@/features/books/components/BookGrid';
+import PopularAuthors from '@/features/books/book-grid/PopularAuthors';
 
 export default function LandingPage() {
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number | null>(
+    null
+  );
   return (
-    <div className='space-y-8 animate-fade-in'>
-      {/* Hero section tampil megah di halaman utama secara publik */}
+    <div className='space-y-8 animate-fade-in px-4'>
       <Hero />
+      <CategoryList
+        selectedCategoryId={selectedCategoryId}
+        onSelectCategory={setSelectedCategoryId}
+      />
 
-      {/* Anda bisa menambahkan info grafis atau buku terpopuler di bawah ini nanti */}
-      <div className='p-8 rounded-2xl bg-zinc-900/40 border border-zinc-800 text-center'>
+      <BookGrid selectedCategoryId={selectedCategoryId} />
+      <PopularAuthors />
+      <div className='p-8 rounded-2xl bg-neutral-200/40 border border-zinc-800 text-center'>
         <p className='text-zinc-400 text-sm font-medium'>
           Please <span className='text-primary font-semibold'>Sign In</span> to
           browse our complete collection and start borrowing books.
