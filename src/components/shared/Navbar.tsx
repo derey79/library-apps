286a24f60ac431c952d219c15d608d5025b7acf1
derey79 <devyreymond@gmail.com>
2026-07-05 22:14:35 +0700

    update for add navbar component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,14 +5,12 @@ import { RootState } from '@/store/store';
 import { logout } from '@/store/slices/authSlice';
 import { ShoppingBag } from 'lucide-react';
 import { User, BookMarked, MessageSquare, LogOut } from 'lucide-react';
-
-// Impor komponen pecahan atomik yang sudah kita buat
 import { NavbarLogo, AuthButtons } from './navbar/NavbarComponents';
 import NavLinks from './navbar/NavLinks';
-import UserMenu from './navbar/UserMenu';
+import NavUserMenu from './navbar/NavUserMenu';
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false); // State untuk mengontrol hamburger menu mobile
+  const [isOpen, setIsOpen] = useState(false);
   const mobileMenuRef = useRef<HTMLDivElement>(null);
 
   const navigate = useNavigate();
@@ -28,7 +26,6 @@ export default function Navbar() {
     navigate('/');
   };
 
-  // Menutup menu mobile otomatis jika pengguna mengklik di luar area menu
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       if (
@@ -43,35 +40,33 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className='border-b border-neutral-100 sticky top-0 z-50 bg-white/80 backdrop-blur-md'>
-      <div className='custom-container h-20 flex items-center justify-between px-6 max-w-7xl mx-auto relative'>
-        {/* LOGO */}
+    <nav className='border-b border-neutral-100 sticky top-0 z-50 bg-white/80 backdrop-blur-md px-4'>
+      <div className='custom-container h-20 flex items-center justify-between mx-auto relative'>
+        {/* logo aplikasi */}
         <NavbarLogo />
 
-        {/* DESKTOP NAV LINKS */}
         {isAuthenticated && <NavLinks />}
 
-        {/* DESKTOP PROFILE / AUTH BUTTONS */}
+        {/* desktop view */}
         <div className='hidden md:flex items-center gap-5'>
           {isAuthenticated ? (
             <>
-              {/* 💡 IKON KERANJANG BELANJA PREMIUM DENGAN BADGE COUNTER */}
-              <Link
-                to='/cart'
-                className='relative p-2.5 rounded-full hover:bg-neutral-50 border border-neutral-100 bg-white transition flex items-center justify-center text-neutral-700 hover:text-neutral-900 group cursor-pointer focus:outline-none select-none'
-              >
-                <ShoppingBag className='h-5 w-5' />
+              {user?.role !== 'ADMIN' && (
+                <Link
+                  to='/cart'
+                  className='relative p-2.5 rounded-full hover:bg-neutral-50 border border-neutral-100 bg-white transition flex items-center justify-center text-neutral-700 hover:text-neutral-900 group cursor-pointer focus:outline-none select-none'
+                >
+                  <ShoppingBag className='h-5 w-5' />
 
-                {/* Gelembung Penanda Jumlah Buku Aktif di Keranjang */}
-                {cartItems.length > 0 && (
-                  <span className='absolute -top-1 -right-1 h-5 w-5 rounded-full bg-blue-600 text-white font-extrabold text-[10px] flex items-center justify-center ring-4 ring-white animate-scale-in'>
-                    {cartItems.length}
-                  </span>
-                )}
-              </Link>
+                  {cartItems.length > 0 && (
+                    <span className='absolute -top-1 -right-1 h-5 w-5 rounded-full bg-red-600 text-white font-extrabold text-[10px] flex items-center justify-center ring-4 ring-white animate-scale-in'>
+                      {cartItems.length}
+                    </span>
+                  )}
+                </Link>
+              )}
 
-              {/* USER PROFILE DROPDOWN */}
-              <UserMenu user={user} onLogout={handleLogout} />
+              <NavUserMenu user={user} onLogout={handleLogout} />
             </>
           ) : (
             <AuthButtons />
@@ -105,36 +100,36 @@ export default function Navbar() {
             className='absolute top-18  w-full bg-white border border-neutral-100 rounded-[24px] shadow-[0_15px_35px_rgba(0,0,0,0.1)] p-3 flex flex-col space-y-1 animate-fade-in md:hidden z-50'
           >
             {isAuthenticated ? (
-              /* KONDISI 1: MOBILE MENU JIKA USER SUDAH LOGIN */
               <>
-                <Link
-                  to='/profile'
-                  onClick={() => setIsOpen(false)}
-                  className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
-                >
-                  <User className='h-4 w-4 Lech text-neutral-400' />
-                  Profile
-                </Link>
+                {user?.role !== 'ADMIN' && (
+                  <>
+                    <Link
+                      to='/profile'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
+                    >
+                      <User className='h-4 w-4 Lech text-neutral-400' />
+                      Profile
+                    </Link>
 
-                <Link
-                  to='/my-loans'
-                  onClick={() => setIsOpen(false)}
-                  className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
-                >
-                  <BookMarked className='h-4 w-4 text-neutral-400' />
-                  Borrowed List
-                </Link>
-
-                <Link
-                  to='/reviews'
-                  onClick={() => setIsOpen(false)}
-                  className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
-                >
-                  <MessageSquare className='h-4 w-4 text-neutral-400' />
-                  Reviews
-                </Link>
-
-                <div className='h-px bg-neutral-100/70 my-1 mx-2' />
+                    <Link
+                      to='/my-loans'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
+                    >
+                      <BookMarked className='h-4 w-4 text-neutral-400' />
+                      Borrowed List
+                    </Link>
+                    <Link
+                      to='/reviews'
+                      onClick={() => setIsOpen(false)}
+                      className='flex items-center gap-3 px-4 py-3.5 text-[15px] font-bold text-neutral-800 hover:bg-neutral-50 rounded-[16px] transition'
+                    >
+                      <MessageSquare className='h-4 w-4 text-neutral-400' />
+                      Reviews
+                    </Link>
+                  </>
+                )}
 
                 <button
                   onClick={handleLogout}
@@ -145,7 +140,6 @@ export default function Navbar() {
                 </button>
               </>
             ) : (
-              /* KONDISI 2: MOBILE MENU JIKA USER BELUM LOGIN */
               <div className='flex flex-col gap-2 p-1'>
                 <AuthButtons onActionClick={() => setIsOpen(false)} />
               </div>
